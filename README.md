3bc3d4b87733ab9058af97a4e4a12794f6d8e0f4
Julián Martín Sasso <usosavazquez@itba.edu.ar>
2025-05-15 13:41:43 -0300

    Try to fix information transfer error

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # GestureDetection
 A controller to detect gestures on motion capute, tested on Motion Capture Laboratory at Intituto Tecnologico de Buenos Aires
+
+# Installation
+To compile in Windows follow these instructions:
+```
+cd build
+cmake --build . --config Release
+```
+
+# Running
+To run on windows (from the root directory of the repository)
+```
+.\build\Release\GestureCaptureController.exe
+```
+
+# Clarifications
+Please make sure to setup OptiTrack information correctly as follows:
+1. OptiTrack server IP: local interface IP
+2. OptiTrack local IP: local interface IP
+
+In motive please ensure you have "Multicast" set.
+
+Example:
--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,22 @@
 // Global flag to control the main loop
 std::atomic<bool> g_running(true);
 
+#define SERVER_IP "10.17.2.79"
+#define LOCAL_IP SERVER_IP
+
 // Callback function that receives data from the NatNet client
 void NATNET_CALLCONV DataHandler(sFrameOfMocapData* data, void* pUserData)
 {
     // Cast user data to our context
     NatNetClient* pClient = (NatNetClient*)pUserData;
 
+  
     // Process skeleton data
     for (int i = 0; i < data->nSkeletons; i++)
     {
         sSkeletonData& skeleton = data->Skeletons[i];
         std::cout << "Skeleton ID: " << skeleton.skeletonID << std::endl;
-        
+
         // Process each rigid body (joint) in the skeleton
         for (int j = 0; j < skeleton.nRigidBodies; j++)
         {
@@ -66,18 +70,19 @@ int main(int argc, char* argv[])
     client->SetFrameReceivedCallback(DataHandler, client);
     NatNet_SetLogCallback(MessageHandler);
     // Connect to Motive (lab IP)
-    std::string serverIP = "127.0.0.1";
-    std::string localIP = "127.0.0.1";  
+    std::string serverIP = SERVER_IP;
+    std::string localIP = LOCAL_IP;  
 
     std::cout << "Connecting to Motive at " << serverIP << "..." << std::endl;
     
     // Initialize client with connection settings
-    ErrorCode result = client->Initialize(localIP.c_str(), serverIP.c_str());
+    ErrorCode result = client->Initialize(localIP.c_str(), serverIP.c_str(), 1510, 1511);
     if (result != ErrorCode_OK)
     {
         std::cout << "Error initializing client. Error code: " << result << std::endl;
         return 1;
     }
+    
     
     // Print client/server info
     sServerDescription ServerDescription;
@@ -101,7 +106,26 @@ int main(int argc, char* argv[])
         while (g_running)
         {
             // Sleep to avoid high CPU usage
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            sFrameOfMocapData* data = client->GetLastFrameOfData();
+
+            std::cout <<"ALGO: " << data->CameraDataReceivedTimestamp;
+
+    // Process skeleton data
+    for (int i = 0; i < data->nSkeletons; i++)
+    {
+        sSkeletonData& skeleton = data->Skeletons[i];
+        std::cout << "Skeleton ID: " << skeleton.skeletonID << std::endl;
+
+        // Process each rigid body (joint) in the skeleton
+        for (int j = 0; j < skeleton.nRigidBodies; j++)
+        {
+            sRigidBodyData& rb = skeleton.RigidBodyData[j];
+            std::cout << "  Joint " << j << ": Position [" 
+                      << rb.x << ", " << rb.y << ", " << rb.z << "]" << std::endl;    
+        }
+    }
+            // DataHandler(data, client);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10000));
         }
     }
     catch (std::exception& e)
