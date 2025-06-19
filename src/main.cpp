82e81a7a77ef7f0c8cb93e7454af81dd5c89c7de
Julián Martín Sasso <usosavazquez@itba.edu.ar>
2025-06-19 12:22:42 -0300

    Downgrade the nat net version
    Co-authored-by: Uriel <urielsv@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,55 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <csignal>
 
 // Include NatNet SDK
 #include "NatNetTypes.h"
-#include "NatNetCAPI.h"
 #include "NatNetClient.h"
+#include <NatNetCAPI.h>
 
 // Global flag to control the main loop
 std::atomic<bool> g_running(true);
 
-#define SERVER_IP "10.17.2.79"
-#define LOCAL_IP SERVER_IP
+// Signal handler for clean termination
+void signalHandler(int signum) {
+    std::cout << "\nInterrupt signal (" << signum << ") received.\n";
+    g_running = false;
+}
 
 // Callback function that receives data from the NatNet client
 void NATNET_CALLCONV DataHandler(sFrameOfMocapData* data, void* pUserData)
 {
     // Cast user data to our context
     NatNetClient* pClient = (NatNetClient*)pUserData;
+    
+    // Print frame number
+    std::cout << "Frame #: " << data->iFrame << std::endl;
 
-  
+    // Process markers (if any)
+    std::cout << "Markers: " << data->nLabeledMarkers << std::endl;
+    for (int i = 0; i < data->nLabeledMarkers; i++)
+    {
+        sMarker& marker = data->LabeledMarkers[i];
+        std::cout << "  Marker " << i << " (ID: " << marker.ID << "): Position ["
+                  << marker.x << ", " << marker.y << ", " << marker.z << "]" << std::endl;
+    }
+
+    // Process rigid bodies (if any)
+    std::cout << "Rigid Bodies: " << data->nRigidBodies << std::endl;
+    for (int i = 0; i < data->nRigidBodies; i++)
+    {
+        sRigidBodyData& rb = data->RigidBodies[i];
+        std::cout << "  RigidBody " << i << " (ID: " << rb.ID << "): Position ["
+                  << rb.x << ", " << rb.y << ", " << rb.z << "]" << std::endl;
+    }
+
     // Process skeleton data
     for (int i = 0; i < data->nSkeletons; i++)
     {
         sSkeletonData& skeleton = data->Skeletons[i];
         std::cout << "Skeleton ID: " << skeleton.skeletonID << std::endl;
-
+        
         // Process each rigid body (joint) in the skeleton
         for (int j = 0; j < skeleton.nRigidBodies; j++)
         {
@@ -35,20 +59,6 @@ void NATNET_CALLCONV DataHandler(sFrameOfMocapData* data, void* pUserData)
             std::cout << "  Joint " << j << ": Position [" 
                       << rb.x << ", " << rb.y << ", " << rb.z << "]" << std::endl;    
         }
-    }
-
-    // Print rigid bodies
-    for (int i = 0; i < data->nRigidBodies; i++) {
-        sRigidBodyData& rb = data->RigidBodies[i];
-        std::cout << "Rigid Body " << i << ": Position [" 
-                  << rb.x << ", " << rb.y << ", " << rb.z << "]" << std::endl;
-    }
-
-    // Print markers
-    for (int i = 0; i < data->nOtherMarkers; i++) {
-        float* marker = data->OtherMarkers[i];
-        std::cout << "Marker " << i << ": Position [" 
-                  << marker[0] << ", " << marker[1] << ", " << marker[2] << "]" << std::endl;
     }
 }
 
@@ -77,26 +87,34 @@ void NATNET_CALLCONV MessageHandler(Verbosity msgType, const char* msg)
 
 int main(int argc, char* argv[])
 {
+    // Register signal handler for clean termination
+    signal(SIGINT, signalHandler);
+    
     // Create NatNet client
     NatNetClient* client = new NatNetClient();
     
     // Set callback handlers
     client->SetFrameReceivedCallback(DataHandler, client);
-    NatNet_SetLogCallback(MessageHandler);
-    // Connect to Motive (lab IP)
-    std::string serverIP = SERVER_IP;
-    std::string localIP = LOCAL_IP;  
-
+    
+    // Connect to Motive
+    std::string serverIP = "127.0.0.1"; // IP address of the Motive/server machine
+    std::string localIP = "127.0.0.1";  // IP address of this machine
+    
     std::cout << "Connecting to Motive at " << serverIP << "..." << std::endl;
     
     // Initialize client with connection settings
-    ErrorCode result = client->Initialize(localIP.c_str(), serverIP.c_str(), 1510, 1511);
+    sNatNetClientConnectParams connectParams;
+    connectParams.connectionType = ConnectionType_Multicast; // or Unicast if needed
+    connectParams.serverCommandPort = 1510;
+    connectParams.serverDataPort = 1511;
+    connectParams.serverAddress = serverIP.c_str();
+    connectParams.localAddress = localIP.c_str();
+    ErrorCode result = client->Connect(connectParams);
     if (result != ErrorCode_OK)
     {
         std::cout << "Error initializing client. Error code: " << result << std::endl;
         return 1;
     }
-    
     
     // Print client/server info
     sServerDescription ServerDescription;
@@ -106,43 +124,56 @@ int main(int argc, char* argv[])
     if (!ServerDescription.HostPresent)
     {
         std::cout << "Unable to connect to Motive server. Exiting." << std::endl;
-        client->Uninitialize();
+        client->Disconnect();
         delete client;
         return 1;
     }
     
     std::cout << "Connected to Motive server: " << ServerDescription.szHostApp << std::endl;
+    std::cout << "Server version: " << ServerDescription.HostAppVersion << std::endl;
+    std::cout << "NatNet version: " << ServerDescription.NatNetVersion << std::endl;
     std::cout << "Press Ctrl+C to exit" << std::endl;
     
     // Main loop
     try
     {
+        std::cout << "Requesting data descriptions..." << std::endl;
+        sDataDescriptions* pDataDefs = NULL;
+        client->GetDataDescriptionList(&pDataDefs);
+        if (pDataDefs)
+        {
+            std::cout << "Retrieved " << pDataDefs->nDataDescriptions << " data descriptions." << std::endl;
+            
+            for (int i = 0; i < pDataDefs->nDataDescriptions; i++)
+            {
+                if (pDataDefs->arrDataDescriptions[i].type == Descriptor_MarkerSet)
+                {
+                    sMarkerSetDescription* pMS = pDataDefs->arrDataDescriptions[i].Data.MarkerSetDescription;
+                    std::cout << "MarkerSet: " << pMS->szName << " (" << pMS->nMarkers << " markers)" << std::endl;
+                }
+                else if (pDataDefs->arrDataDescriptions[i].type == Descriptor_RigidBody)
+                {
+                    sRigidBodyDescription* pRB = pDataDefs->arrDataDescriptions[i].Data.RigidBodyDescription;
+                    std::cout << "RigidBody: " << pRB->szName << " (ID: " << pRB->ID << ")" << std::endl;
+                }
+            }
+            
+            NatNet_FreeDescriptions(pDataDefs);
+        }
+        else
+        {
+            std::cout << "Unable to retrieve data descriptions." << std::endl;
+        }
+        
+        std::cout << "\nStarting main loop. Data will be printed as it arrives." << std::endl;
+        std::cout << "Press Ctrl+C to exit cleanly.\n" << std::endl;
+        
         while (g_running)
         {
+            client->GetDataDescriptionList(&pDataDefs);
+            
+            
             // Sleep to avoid high CPU usage
-            sFrameOfMocapData* data = client->GetLastFrameOfData();
-
-            if (data == nullptr) {
-            std::cout << "No data received this frame" << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            continue;
-        }
-
-    // Process skeleton data
-    for (int i = 0; i < data->nSkeletons; i++)
-    {
-        sSkeletonData& skeleton = data->Skeletons[i];
-        std::cout << "Skeleton ID: " << skeleton.skeletonID << std::endl;
-
-        // Process each rigid body (joint) in the skeleton
-        for (int j = 0; j < skeleton.nRigidBodies; j++)
-        {
-            sRigidBodyData& rb = skeleton.RigidBodyData[j];
-            std::cout << "  Joint " << j << ": Position [" 
-                      << rb.x << ", " << rb.y << ", " << rb.z << "]" << std::endl;    
-        }
-    }
-            // DataHandler(data, client);
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     }
@@ -153,7 +184,7 @@ int main(int argc, char* argv[])
     
     // Clean up
     std::cout << "Shutting down..." << std::endl;
-    client->Uninitialize();
+    client->Disconnect();
     delete client;
     
     return 0;
